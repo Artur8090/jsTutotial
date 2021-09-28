9fbbe34537e70a0ba6f5943c568163c3668608fa
Artur8090 <artur.cheuchiev@gmail.com>
2021-09-28 19:34:24 +0200

    tho

---
+++ alert.js/variables.js
@@ -0,0 +1,17 @@
+var m3$$3g3 = "Hello"
+alert(m3$$3g3)
+var v4ri4bl3 = "new variable"
+alert(v4ri4bl3)
+
+var $ = 14;
+var _ = 18;
+alert($ + _)
+
+var name = "Василий"
+var admin = "Василий"
+
+alert(admin)
+
+var ourPlanet = "земля"
+var Visitor = " с юрой"
+alert(ourPlanet + Visitor)
