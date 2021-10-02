8816570d51b7c7e355d512cf70385793e3f90871
Artur8090 <artur.cheuchiev@gmail.com>
2021-10-02 14:03:09 +0200

    tho

--- alert.js/alert.js
+++ alert.js/alert.js
@@ -1,4 +1,8 @@
-alert("привет мир");
-alert("амогус");
-alert("ඞ");
 
+
+var years = prompt("сколько вам лет?");
+alert(  "вам " + years);
+var isAdmin = confirm("вы - админ?");
+alert( isAdmin );
+if ( years < 20) { alert("вы не можете быть на этом сайте");
+}
