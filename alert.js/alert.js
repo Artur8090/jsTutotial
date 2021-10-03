eb5258e3e5fcbb8df962a2168102d39019d3a97a
Artur8090 <artur.cheuchiev@gmail.com>
2021-10-03 15:03:30 +0200

    okt3

--- alert.js/alert.js
+++ alert.js/alert.js
@@ -17,3 +17,27 @@ if(question == "EcmaScript") {
     alert("нет не правильно!")
 }
  
+var number = prompt("выведитье число", 0)
+if(number < 0) {
+    alert(-1)
+} else if( number > 0) {
+alert(1)
+} else {
+    alert(0)
+}
+
+var accses = prompt("кто пришел?")
+if(accses == "admin") {
+    var password = prompt("напишыте пароль")
+    if(password == "artur8090!") {
+        alert("добро пожалывать")
+    } else if(password == null){
+        alert("отменено")
+    } else{
+        alert("не правильный пароль")
+    }
+} else if(accses == null) {
+alert("процесс был отменен")
+} else {
+    alert("не верный username")
+}
