00ead3d703e6996647914a9e6b49bdcd258b818d
Artur8090 <artur.cheuchiev@gmail.com>
2021-10-06 14:47:49 +0200

    okt6

---
+++ alert.js/forAndWhile.js
@@ -0,0 +1,20 @@
+var i = 3;
+while (i) {
+   alert( i-- )
+}
+
+var i2 = 0;
+while(++i2 < 5) alert(i2);
+
+
+for(var i3 = 2; i3 <= 10;i3++){
+    if(i3 % 2 == 0)
+alert(i3);
+}
+nextPrime:
+for(var newI = 2; newI < 20; newI++){
+    for(var j = 2; j < newI; j++){
+        if(newI % j == 0) continue nextPrime;
+    }
+    alert(newI);
+}
