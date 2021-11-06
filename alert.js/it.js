cce291dadd29fe1c38f4b702c158ca742862a700
Artur8090 <artur.cheuchiev@gmail.com>
2021-11-06 17:05:54 +0100

    nov6

--- alert.js/it.js
+++ alert.js/it.js
@@ -1,3 +1,4 @@
+/*
 var priceA = 0.2, priceB = 0.1;
 
 alert(+(priceA + priceB).toFixed(2));
@@ -18,3 +19,24 @@ return b;
 }
 
 alert(fiboNet(20))
+
+
+
+
+var max = 100;
+
+
+
+alert(Math.random()* max)
+
+var min = 15, Max = 20;
+
+alert(Math.random()* (Max - min));
+*/
+
+function randomInteger(min, max) {
+    var num = min + Math.random() * (max - min)
+    num = Math.round(num);
+    return num;
+}
+alert(randomInteger(1,3));
