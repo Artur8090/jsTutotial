01c0fd3af5e303c4616b9b6d02d6bf19d0a5b599
Artur8090 <artur.cheuchiev@gmail.com>
2021-11-07 19:58:58 +0100

    nov7

---
+++ alert.js/numbers/fibNet.js
@@ -0,0 +1,21 @@
+function fibNet (n) {
+    var f = (1 + Math.sqrt(5)) / 2;
+    return Math.round(Math.pow(f, n)) / Math.sqrt(5)
+
+};
+
+alert(fibNet(77));
+
+function fib(n) {
+    var a = 1,
+     b = 0, 
+     x;
+     for(var i = 0; i < n; i++) {
+         x = a + b;
+         a = b;
+         b = x;
+     }
+return b;
+    }
+
+alert(fib(77))
