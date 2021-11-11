eff4acaab3213c8310943b3a12e33ebd2f42583c
Artur8090 <artur.cheuchiev@gmail.com>
2021-11-11 16:51:10 +0100

    nov11

---
+++ alert.js/numbers/decimal.js
@@ -0,0 +1,5 @@
+
+
+alert(6.32.toFixed(1));
+
+alert(6.35.toFixed(20))
---
+++ alert.js/numbers/wrongLoop.js
@@ -0,0 +1,17 @@
+//wrong loop
+var i = 0;
+while (i != 10) {
+    i+= 0.2;
+}
+alert(i)
+
+//right loop
+
+var e = 0;
+while(e < 11) {
+e += 0.2;
+if(e > 9.6 && e < 10.4) {
+    alert(e)
+}
+
+}
