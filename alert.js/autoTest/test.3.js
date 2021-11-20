8e1edd62f38713b2848618cbce369cf7fe44a9a9
Artur8090 <artur.cheuchiev@gmail.com>
2021-11-20 17:15:21 +0100

    nov20

---
+++ alert.js/autoTest/test.3.js
@@ -0,0 +1,16 @@
+describe("pow", function(){
+    it("возводит 5 в степень 1", function() {
+        assert.equal(pow(5,1), 5)
+    })
+
+    it("возводит 5 в степень 2", function() {
+        assert.equal(pow(5,2), 25)
+    })
+
+    it("возводит 5 в степень 3", function() {
+        assert.equal(pow(5,3), 125)
+    })
+    
+    
+    
+})
